dc0a91e7f35b39452c1d24124d9b7b30128b22cb
Ildar Sagdejev <specious@gmail.com>
2014-08-11 01:42:25 -0400

    Added watch capability to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,26 @@ var gulp   = require('gulp'),
     nib    = require('nib'),
     jade   = require('gulp-jade')
 
+var paths = {
+  styles: ['./css/flower.styl', './css/main.styl'],
+  jades: './index.jade'
+}
+
+gulp.task( 'watch', function() {
+  gulp.watch( paths.styles, ['styles'] )
+  gulp.watch( paths.jades, ['jade'] )
+} )
+
 gulp.task( 'styles', function () {
-  gulp.src( ['./css/flower.styl', './css/main.styl'] )
+  gulp.src( paths.styles )
     .pipe( stylus( { use: [nib()] } ) )
     .pipe( gulp.dest( './css' ) )
 } )
 
 gulp.task( 'jade', function () {
-  gulp.src( ['./index.jade'] )
+  gulp.src( paths.jades )
     .pipe( jade( { pretty: true } ) )
     .pipe( gulp.dest( './' ) )
 } )
 
-gulp.task( 'default', ['styles', 'jade'] )
+gulp.task( 'default', ['watch', 'styles', 'jade'] )
