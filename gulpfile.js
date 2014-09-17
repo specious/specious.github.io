7a877fb4b1efa0e9ff0aae2a4e92a8912084c082
Ildar Sagdejev <specious@gmail.com>
2014-09-16 23:05:18 -0400

    Minify CSS and pull into HTML file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
-var gulp   = require('gulp'),
-    stylus = require('gulp-stylus'),
-    nib    = require('nib'),
-    jade   = require('gulp-jade')
+var gulp    = require('gulp'),
+    stylus  = require('gulp-stylus'),
+    nib     = require('nib'),
+    jade    = require('gulp-jade'),
+    minicss = require('gulp-minify-css'),
+    concat  = require('gulp-concat'),
+    fs      = require('fs'),
+    repl    = require('gulp-replace')
 
 var paths = {
   styles: ['./css/flower.styl', './css/main.styl'],
@@ -15,13 +19,19 @@ gulp.task( 'watch', function() {
 
 gulp.task( 'styles', function () {
   gulp.src( paths.styles )
-    .pipe( stylus( { use: [nib()] } ) )
+    .pipe( stylus( { use: [ nib() ] } ) )
+    .pipe( concat( 'styles.css' ) )
+    .pipe( minicss() )
     .pipe( gulp.dest( './css' ) )
 } )
 
 gulp.task( 'jade', function () {
   gulp.src( paths.jades )
     .pipe( jade( { pretty: true } ) )
+    // Import CSS: http://stackoverflow.com/questions/23820703/how-to-inject-content-of-css-file-into-html-in-gulp
+    .pipe( repl( /<link rel="stylesheet" href="(.*\.css)">/g, function( s, file ) {
+        return '<style>' + fs.readFileSync( file, 'utf8' ) + '</style>'
+    } ) )
     .pipe( gulp.dest( './' ) )
 } )
 
